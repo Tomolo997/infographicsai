4144896c5061672673d4a34c48e5798f90200c99
Ovsenjak <tomazovsenjak7@gmail.com>
2025-11-29 10:13:17 +0100

    x

--- front/server/routes/sitemap.xml.ts
+++ front/server/routes/sitemap.xml.ts
@@ -1,4 +1,7 @@
-export default defineEventHandler((event) => {
+import { readdir } from 'fs/promises'
+import { join } from 'path'
+
+export default defineEventHandler(async (event) => {
   const baseUrl = 'https://ainfographic.com'
   const currentDate = new Date().toISOString().split('T')[0]
 
@@ -12,18 +15,43 @@ export default defineEventHandler((event) => {
     { url: '/terms', priority: '0.5', changefreq: 'yearly' },
   ]
 
+  // Dynamically get all blog posts
+  const blogPages: Array<{ url: string; priority: string; changefreq: string }> = []
+  try {
+    const blogDir = join(process.cwd(), 'pages', 'blog')
+    const files = await readdir(blogDir)
+
+    // Filter out index.vue and get all other .vue files
+    const blogPosts = files.filter(file => file.endsWith('.vue') && file !== 'index.vue')
+
+    blogPosts.forEach(file => {
+      // Remove .vue extension to get the slug
+      const slug = file.replace('.vue', '')
+      blogPages.push({
+        url: `/blog/${slug}`,
+        priority: '0.8',
+        changefreq: 'monthly'
+      })
+    })
+  } catch (error) {
+    console.error('Error reading blog directory:', error)
+  }
+
+  // Combine all pages
+  const allPages = [...publicPages, ...blogPages]
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${publicPages
-  .map(
-    (page) => `  <url>
+${allPages
+      .map(
+        (page) => `  <url>
     <loc>${baseUrl}${page.url}</loc>
     <lastmod>${currentDate}</lastmod>
     <changefreq>${page.changefreq}</changefreq>
     <priority>${page.priority}</priority>
   </url>`
-  )
-  .join('\n')}
+      )
+      .join('\n')}
 </urlset>`
 
   event.node.res.setHeader('Content-Type', 'application/xml')
