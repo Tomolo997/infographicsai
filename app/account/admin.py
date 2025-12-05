f077b9e2ef0957cb945ebe72f57d0d7393cc54a3
Ovsenjak <tomazovsenjak7@gmail.com>
2025-12-05 07:13:02 +0100

    added credit purchases to admin

--- app/account/admin.py
+++ app/account/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from account.models import Account, CreditPack, CustomUser
+from account.models import Account, CreditPack, CreditPurchase, CustomUser
 
 admin.site.register(Account)
 admin.site.register(CustomUser)
 admin.site.register(CreditPack)
+admin.site.register(CreditPurchase)
